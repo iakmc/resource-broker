26b3a867aa270fec1bf12649a689741798646973
Nelo-T. Wallus <n.wallus@sap.com>
2025-11-05 14:02:51 +0100

    Add brokerv1alpha1.Status
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

---
+++ api/broker/v1alpha1/status.go
@@ -0,0 +1,43 @@
+// Copyright 2025.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+// Status represents the status of a resource.
+type Status string
+
+const (
+	// StatusUnknown indicates that the resource has not been processed
+	// yet.
+	StatusUnknown Status = "Unknown"
+
+	// StatusProvisioning indicates that the resource is being
+	// provisioned by a provider.
+	StatusProvisioning Status = "Provisioning"
+
+	// StatusAvailable indicates that the provider has finished
+	// provisioning the resource and it is available for use.
+	StatusAvailable Status = "Available"
+
+	// StatusDegraded indicates that the resource is in a degraded
+	// state.
+	StatusDegraded Status = "Degraded"
+
+	// StatusDeleting indicates that the resource is being deleted.
+	StatusDeleting Status = "Deleting"
+
+	// StatusFailed indicates that the resource has failed.
+	StatusFailed Status = "Failed"
+)
