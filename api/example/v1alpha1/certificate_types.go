9f65da7ec193069000b7db855f20a5acabaae46f
Nelo-T. Wallus <n.wallus@sap.com>
2025-11-21 09:36:51 +0100

    codegen
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- api/example/v1alpha1/certificate_types.go
+++ api/example/v1alpha1/certificate_types.go
@@ -1,11 +1,27 @@
+// Copyright 2025.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package v1alpha1
 
 import (
-	brokerv1alpha1 "github.com/platform-mesh/resource-broker/api/broker/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	brokerv1alpha1 "github.com/platform-mesh/resource-broker/api/broker/v1alpha1"
 )
 
-// CertificateSpec defines the desired state of Certificate
+// CertificateSpec defines the desired state of Certificate.
 type CertificateSpec struct {
 	// fqdn is the fully qualified domain name for the certificate.
 	// +required
@@ -36,7 +52,7 @@ type CertificateStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
-// Certificate is the Schema for the certificates API
+// Certificate is the Schema for the certificates API.
 type Certificate struct {
 	metav1.TypeMeta `json:",inline"`
 
@@ -55,7 +71,7 @@ type Certificate struct {
 
 // +kubebuilder:object:root=true
 
-// CertificateList contains a list of Certificate
+// CertificateList contains a list of Certificate.
 type CertificateList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitzero"`
